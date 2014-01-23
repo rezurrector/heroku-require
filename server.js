bed8aaee7274b6ec5b2b62ede5eb18602b1829c3
angel marquez <angel.marquez@gmail.com>
2014-01-22 22:36:34 -0800

    connection string

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express'),
 var app = express();
 
 app.configure(function () {
-    app.set('port', process.env.PORT || 3020);
+    app.set('port', process.env.PORT || 3000);
     app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser());
     app.use(express.static(path.join(__dirname, (process.env.NODE_ENV === 'production') ? 'public-built' : 'public')));
